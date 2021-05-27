16696f57bbf6a838a4426496cf2b3c37f0bc0512
Luis Landkoer <llandkoer@gmail.com>
2021-05-27 11:18:49 -0500

    Create application

---
+++ api/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const morgan = require("morgan");
+
+const helmet = require("helmet");
+
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+const app = express();
+
+const authRoute = require("./routes/authRoutes");
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(limiter);
+
+// Routes
+app.use("/api/v1.0/users", authRoute);
+
+module.exports = app;
