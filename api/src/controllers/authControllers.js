c2b958db297b2384092203b5f555673e9a780162
Luis Landkoer <llandkoer@gmail.com>
2021-06-07 21:54:33 -0500

    Create signup route

--- api/src/controllers/authControllers.js
+++ api/src/controllers/authControllers.js
@@ -1,12 +1,45 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const { nanoid } = require("nanoid");
 
 const { config } = require("../config/config");
 
-const createUser = async (req, res) => {}
+const createUser = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
 
-const loginUser = async (req, res) => {}
+    let { name, email, username, password } = req.body;
+
+    const isAdmin = false;
+
+    // "SELECT" to see if it already exists
+    const user;
+
+    if (user.length !== 0) {
+      return res.status(409).json({ message: "Username or email already exist on our database" });
+    }
+
+    // Create User ID
+    const user_id = nanoid();
+
+
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+    // "INSERT" in DB if everything is ok
+
+
+    res.status(201).json({ message: "User has been created successfully" });
+  } catch (error) {
+    res.status(500).json({ error, message: "There was a server error" });
+  }
+};
+
+const loginUser = async (req, res) => {};
 
 exports.createUser = createUser;
 exports.loginUser = loginUser;
--- api/src/middlewares/authMiddlewares.js
+++ api/src/middlewares/authMiddlewares.js
@@ -1,5 +1,30 @@
 const { check } = require("express-validator");
 
-exports.signupValidations = () => {};
+exports.signupValidations = () => [
+  check("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 4, max: 100 })
+    .withMessage("Name must be between 4 and 100 characters long"),
+  check("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Email is not valid")
+    .isLength({ max: 255 })
+    .withMessage("Email cannot be longer than 255 characters"),
+  check("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 4, max: 50 })
+    .withMessage("Username must be between 4 and 50 characters long"),
+  check("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isStrongPassword({ minLength: 10, minLowercase: 1, minUppercase: 1, minNumbers: 1 })
+    .withMessage("Password must be at least 10 characters long and must contain at least 1 uppercase character, 1 lowercase character and 1 number")
+    .isLength({ max: 100 })
+    .withMessage("Password cannot be longer than 100 characters"),
+];
 
 exports.loginValidations = () => {};
