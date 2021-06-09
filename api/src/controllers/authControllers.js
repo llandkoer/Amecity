27f75067118ee1ff1bd6aad6c919510b48180547
Luis Landkoer <llandkoer@gmail.com>
2021-06-09 12:40:27 -0500

    Update signup controller

--- api/src/controllers/authControllers.js
+++ api/src/controllers/authControllers.js
@@ -1,9 +1,11 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
 const { nanoid } = require("nanoid");
 
-const { config } = require("../config/config");
+// const { config } = require("../config/config");
+
+const { getConnection } = require("../database");
 
 const createUser = async (req, res) => {
   try {
@@ -12,34 +14,36 @@ const createUser = async (req, res) => {
       return res.status(422).json({ errors: errors.array() });
     }
 
-    let { name, email, username, password } = req.body;
+    let { name, email, username, is_admin, password } = req.body;
 
-    const isAdmin = false;
-
-    // "SELECT" to see if it already exists
-    const user;
-
-    if (user.length !== 0) {
-      return res.status(409).json({ message: "Username or email already exist on our database" });
+    const user = getConnection().get("users").find({ email, username }).value();
+    if (user) {
+      return res.status(409).json({ message: "Email or username already exist on our database" });
     }
 
-    // Create User ID
     const user_id = nanoid();
-
 
     const salt = await bcrypt.genSalt(10);
     password = await bcrypt.hash(password, salt);
 
-    // "INSERT" in DB if everything is ok
-
+    const newUser = {
+      user_id,
+      name,
+      email,
+      username,
+      is_admin,
+      password,
+    };
+    getConnection().get("users").push(newUser).write();
 
     res.status(201).json({ message: "User has been created successfully" });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error, message: "There was a server error" });
   }
 };
 
-const loginUser = async (req, res) => {};
+// const loginUser = async (req, res) => {};
 
 exports.createUser = createUser;
-exports.loginUser = loginUser;
+// exports.loginUser = loginUser;
