5f84ad799397cb4c16add11d14bb9c10f869dc4f
Luis Landkoer <75341924+llandkoer@users.noreply.github.com>
2021-06-09 15:44:15 -0500

    Feat: create API signup route  (#24)
    * Update dev script
    
    * Create signup route
    
    * Ignore package-lock.json
    
    * Update dev script
    
    * Create initial database setup
    
    * Update signup validations
    
    * Update lowdb version and dev script
    
    * Update database setup
    
    * Update signup controller
    
    * Update auth routes
    
    * Add database file
    
    * Style signup controller
    
    * Restyle Feat: create API signup route  (#25)
    
    * Restyled by clang-format
    
    * Restyled by jq
    
    * Restyled by prettier
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- api/src/controllers/authControllers.js
+++ api/src/controllers/authControllers.js
@@ -1,12 +1,50 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const { nanoid } = require("nanoid");
 
-const { config } = require("../config/config");
+// const { config } = require("../config/config");
 
-const createUser = async (req, res) => {}
+const { getConnection } = require("../database");
 
-const loginUser = async (req, res) => {}
+const createUser = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let { name, email, username, is_admin, password } = req.body;
+
+    const user = getConnection().get("users").find({ email, username }).value();
+    if (user) {
+      return res
+        .status(409)
+        .json({ message: "Email or username already exist on our database" });
+    }
+
+    const user_id = nanoid();
+
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+    const newUser = {
+      user_id,
+      name,
+      email,
+      username,
+      is_admin,
+      password,
+    };
+    getConnection().get("users").push(newUser).write();
+
+    res.status(201).json({ message: "User has been created successfully" });
+  } catch (error) {
+    res.status(500).json({ error, message: "There was a server error" });
+  }
+};
+
+// const loginUser = async (req, res) => {};
 
 exports.createUser = createUser;
-exports.loginUser = loginUser;
+// exports.loginUser = loginUser;
