62aebab257b1f3a44472ad5b73073e8519b13fea
Restyled.io <commits@restyled.io>
2021-06-09 17:48:38 +0000

    Restyled by prettier

--- api/src/middlewares/authMiddlewares.js
+++ api/src/middlewares/authMiddlewares.js
@@ -1,33 +1,33 @@
-const {check} = require("express-validator");
+const { check } = require("express-validator");
 
-exports.signupValidations = () =>
-    [check("name")
-         .notEmpty()
-         .withMessage("Name is required")
-         .isLength({min : 4, max : 100})
-         .withMessage("Name must be between 4 and 100 characters long"),
-     check("email")
-         .notEmpty()
-         .withMessage("Email is required")
-         .isEmail()
-         .withMessage("Email is not valid")
-         .isLength({max : 255})
-         .withMessage("Email cannot be longer than 255 characters"),
-     check("username")
-         .notEmpty()
-         .withMessage("Username is required")
-         .isLength({min : 4, max : 50})
-         .withMessage("Username must be between 4 and 50 characters long"),
-     check("is_admin")
-         .notEmpty()
-         .withMessage("Value is required")
-         .isBoolean()
-         .withMessage("Value must be boolean"),
-     check("password")
-         .notEmpty()
-         .withMessage("Password is required")
-         .isLength({min : 10, max : 100})
-         .withMessage("Password must be between 10 and 100 characters long"),
+exports.signupValidations = () => [
+  check("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 4, max: 100 })
+    .withMessage("Name must be between 4 and 100 characters long"),
+  check("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Email is not valid")
+    .isLength({ max: 255 })
+    .withMessage("Email cannot be longer than 255 characters"),
+  check("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 4, max: 50 })
+    .withMessage("Username must be between 4 and 50 characters long"),
+  check("is_admin")
+    .notEmpty()
+    .withMessage("Value is required")
+    .isBoolean()
+    .withMessage("Value must be boolean"),
+  check("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 10, max: 100 })
+    .withMessage("Password must be between 10 and 100 characters long"),
 ];
 
 exports.loginValidations = () => {};
--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -1,12 +1,15 @@
-const {Router} = require("express");
+const { Router } = require("express");
 
 const router = Router();
 
 const authController = require("../controllers/authControllers");
 const authMiddlewares = require("../middlewares/authMiddlewares");
 
-router.post("/signup", authMiddlewares.signupValidations(),
-            authController.createUser);
+router.post(
+  "/signup",
+  authMiddlewares.signupValidations(),
+  authController.createUser
+);
 
 // router.post("/login", authMiddlewares.loginValidations(),
 // authController.loginUser);
