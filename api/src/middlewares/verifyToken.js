ca04dc8941dc279a00e7995298441d71e44fa3f6
Restyled.io <commits@restyled.io>
2021-06-09 21:39:44 +0000

    Restyled by clang-format

--- api/src/middlewares/verifyToken.js
+++ api/src/middlewares/verifyToken.js
@@ -1,27 +1,27 @@
 const jwt = require("jsonwebtoken");
 
-const { config } = require("../config/config");
+const {config} = require("../config/config");
 
 const verifyToken = (isAdmin, label) => (req, res, next) => {
   try {
     const token = req.headers["x-access-token"];
     if (!token) {
-      return res.status(401).json({ message: "No token provided" });
+      return res.status(401).json({message : "No token provided"});
     }
 
     jwt.verify(token, config.jwt.secretKey, (error, decoded) => {
       if (error) {
-        return res.status(401).json({ message: "Token is invalid" });
+        return res.status(401).json({message : "Token is invalid"});
       }
 
       if (decoded.admin !== isAdmin) {
-        return res.status(401).json({ message: `User is not ${label}` });
+        return res.status(401).json({message : `User is not ${label}`});
       }
       req.body.id = decoded.id;
       next();
     });
   } catch (error) {
-    res.status(500).json({ error, message: "There was an error on server" });
+    res.status(500).json({error, message : "There was an error on server"});
   }
 };
 
