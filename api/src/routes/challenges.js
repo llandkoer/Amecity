f368a77e78e910b9719a7f169475c02610fcd763
Restyled.io <commits@restyled.io>
2021-06-24 03:55:02 +0000

    Restyled by prettier

--- api/src/routes/challenges.js
+++ api/src/routes/challenges.js
@@ -1,29 +1,37 @@
-const {Router} = require("express");
-const {config} = require("../config/config");
+const { Router } = require("express");
+const { config } = require("../config/config");
 
 const Challenges = require("../controllers/challenges");
 const verifyToken = require("../middlewares/verifyToken");
 
 const router = Router();
 
-router.get("/getAll", verifyToken.verifyToken(config.jwt.player.label),
-           Challenges.getAllChallenges);
+router.get(
+  "/getAll",
+  verifyToken.verifyToken(config.jwt.player.label),
+  Challenges.getAllChallenges
+);
 
-router.post("/takeChallenge", verifyToken.verifyToken(config.jwt.player.label),
-            Challenges.takeChallenge);
+router.post(
+  "/takeChallenge",
+  verifyToken.verifyToken(config.jwt.player.label),
+  Challenges.takeChallenge
+);
 
 // US4
-router.put("/achieveChallenge",
-           verifyToken.verifyToken(config.jwt.player.label),
-           Challenges.achieveChallenge);
+router.put(
+  "/achieveChallenge",
+  verifyToken.verifyToken(config.jwt.player.label),
+  Challenges.achieveChallenge
+);
 
 router.get(
-    "/getAchieved",
-    // verifyToken.verifyToken(config.jwt.player.label),
-    Challenges.getAchieved
-    // Verificar que el usuario esté loggeado
-    // Traer id usuario
-    // Traer retos where id = 1
-)
+  "/getAchieved",
+  // verifyToken.verifyToken(config.jwt.player.label),
+  Challenges.getAchieved
+  // Verificar que el usuario esté loggeado
+  // Traer id usuario
+  // Traer retos where id = 1
+);
 
 module.exports = router;
