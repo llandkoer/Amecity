98744ffe7d505e185b595271afeff2b28033e577
Restyled.io <commits@restyled.io>
2021-06-24 03:02:54 +0000

    Restyled by clang-format

--- api/src/routes/challenges.js
+++ api/src/routes/challenges.js
@@ -1,36 +1,30 @@
-const { Router } = require("express");
-const { config } = require("../config/config");
+const {Router} = require("express");
+const {config} = require("../config/config");
 
 const Challenges = require("../controllers/challenges");
 const verifyToken = require("../middlewares/verifyToken");
 
 const router = Router();
 
-router.get(
-    "/getAll", 
-    verifyToken.verifyToken(config.jwt.player.label),
-    Challenges.getAllChallenges 
-);
+router.get("/getAll", verifyToken.verifyToken(config.jwt.player.label),
+           Challenges.getAllChallenges);
 
-router.post(
-    "/takeChallenge", 
-    verifyToken.verifyToken(config.jwt.player.label),
-    Challenges.takeChallenge
-);
+router.post("/takeChallenge", verifyToken.verifyToken(config.jwt.player.label),
+            Challenges.takeChallenge);
 
-// US4 
+// US4
 router.put("/achieveChallenge", async (req, res) => {
-    // Verificar que el usuario esté loggeado
-    // Traer id usuario
-    // Traer id reto seleccionado
-    // Asignar status reto = 1
+                                    // Verificar que el usuario esté loggeado
+                                    // Traer id usuario
+                                    // Traer id reto seleccionado
+                                    // Asignar status reto = 1
 
-});
+                                });
 
 router.get("/getAchieved", async (req, res) => {
-    // Verificar que el usuario esté loggeado
-    // Traer id usuario
-    // Traer retos where id = 1
-})
+                               // Verificar que el usuario esté loggeado
+                               // Traer id usuario
+                               // Traer retos where id = 1
+                           })
 
 module.exports = router;
