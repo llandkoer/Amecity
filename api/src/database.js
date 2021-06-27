3000a4af92ea474bc4e763a97539e2f28950aa73
Luis Landkoer <llandkoer@gmail.com>
2021-06-27 18:00:55 -0500

    Update database settings

--- api/src/database.js
+++ api/src/database.js
@@ -6,7 +6,7 @@ let db;
 async function createConnection() {
   const adapter = new FileAsync("db.json");
   db = await low(adapter);
-  db.defaults({ users: [], challenges: [], partners: [] }).write();
+  db.defaults({ users: [], challenges: [], partners: [], posts: [], providers: [], current_challenges: [], completed_challenges: [] }).write();
 }
 
 const getConnection = () => db;
