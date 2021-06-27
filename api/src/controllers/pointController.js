622c20b62e3df5eb0e394c6e3fa9fc0d653c5c98
Restyled.io <commits@restyled.io>
2021-06-27 14:08:23 +0000

    Restyled by clang-format

--- api/src/controllers/pointController.js
+++ api/src/controllers/pointController.js
@@ -1,39 +1,39 @@
-const { getConnection } = require("../database");
+const {getConnection} = require("../database");
 
 const getPoints = async (req, res) => {
   try {
-    const { id } = req.body;
-    const user = getConnection().get("users").find({ user_id: id }).value();
-    if (!user) return res.status(400).json({ message: "Token no valid" });
-    res.status(200).json({ points: user.points });
+    const {id} = req.body;
+    const user = getConnection().get("users").find({user_id : id}).value();
+    if (!user)
+      return res.status(400).json({message : "Token no valid"});
+    res.status(200).json({points : user.points});
   } catch (error) {
-    res.status(500).json({ error, message: "There was a server error" });
+    res.status(500).json({error, message : "There was a server error"});
   }
 };
 
 const redimePoints = async (req, res) => {
   try {
-    const { id } = req.body;
-    const user = getConnection().get("users").find({ user_id: id }).value();
-    if (!user) return res.status(400).json({ message: "Token no valid" });
+    const {id} = req.body;
+    const user = getConnection().get("users").find({user_id : id}).value();
+    if (!user)
+      return res.status(400).json({message : "Token no valid"});
     if (!req.headers.points || req.headers.points <= 0)
-      return res.status(400).json({ message: "Imcomplete Data" });
+      return res.status(400).json({message : "Imcomplete Data"});
     const updatePoints = parseInt(user.points) - parseInt(req.headers.points);
     if (updatePoints < 0) {
-      return res
-        .status(400)
-        .json({ message: "Insufficient points", points: user.points });
+      return res.status(400).json(
+          {message : "Insufficient points", points : user.points});
     }
     getConnection()
-     .get("users")
-     .find({ user_id: id })
-     .assign({ points: updatePoints })
-     .write();
-    return res
-      .status(200)
-      .json({ message: "Points has update", points: updatePoints });
+        .get("users")
+        .find({user_id : id})
+        .assign({points : updatePoints})
+        .write();
+    return res.status(200).json(
+        {message : "Points has update", points : updatePoints});
   } catch (error) {
-    return res.status(500).json({ error, message: "There was a server error" });
+    return res.status(500).json({error, message : "There was a server error"});
   }
 };
 
