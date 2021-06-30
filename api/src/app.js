733f04840849b7fa778be962bf45418a85782018
Restyled.io <commits@restyled.io>
2021-06-30 02:28:09 +0000

    Restyled by prettier

--- api/src/app.js
+++ api/src/app.js
@@ -7,24 +7,24 @@ const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs : 15 * 60 * 1000,
-  max : 100,
+  windowMs: 15 * 60 * 1000,
+  max: 100,
 });
 
 const app = express();
 
 const authRoute = require("./routes/authRoutes");
-const challenges = require("./routes/challengesRoutes")
+const challenges = require("./routes/challengesRoutes");
 
 // Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(limiter);
 
 // Routes
 app.use("/api/auth/", authRoute);
-app.use("/api/challenges/", challenges)
+app.use("/api/challenges/", challenges);
 
 module.exports = app;
