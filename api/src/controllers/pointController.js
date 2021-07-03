13a2cc273b60ec90891807961b893412f60103e9
Villadiego Hoyos <d.villadiego.hoyos@accenture.com>
2021-07-03 10:03:45 -0500

    fixed: Luis Landkoer suggestion implemented

--- api/src/controllers/pointController.js
+++ api/src/controllers/pointController.js
@@ -2,9 +2,9 @@ const { getConnection } = require("../database");
 
 const getPoints = async (req, res) => {
   try {
-    const { id } = req.body;
-    const user = getConnection().get("users").find({ user_id: id }).value();
-    if (!user) return res.status(400).json({ message: "Token no valid" });
+    const { user_id } = req.body;
+    const user = getConnection().get("users").find({ user_id: user_id }).value();
+    if (!user) return res.status(400).json({ message: "This user doest not exist on our database" });
     res.status(200).json({ points: user.points });
   } catch (error) {
     res.status(500).json({ error, message: "There was a server error" });
@@ -13,12 +13,12 @@ const getPoints = async (req, res) => {
 
 const redimePoints = async (req, res) => {
   try {
-    const { id } = req.body;
-    const user = getConnection().get("users").find({ user_id: id }).value();
+    const { user_id } = req.body;
+    const user = getConnection().get("users").find({ user_id: user_id }).value();
     if (!user) return res.status(400).json({ message: "Token no valid" });
-    if (!req.headers.points || req.headers.points <= 0)
+    if (!req.body.points || req.body.points <= 0)
       return res.status(400).json({ message: "Imcomplete Data" });
-    const updatePoints = parseInt(user.points) - parseInt(req.headers.points);
+    const updatePoints = parseInt(user.points) - parseInt(req.body.points);
     if (updatePoints < 0) {
       return res
         .status(400)
@@ -26,7 +26,7 @@ const redimePoints = async (req, res) => {
     }
     getConnection()
       .get("users")
-      .find({ user_id: id })
+      .find({ user_id: user_id })
       .assign({ points: updatePoints })
       .write();
     return res
