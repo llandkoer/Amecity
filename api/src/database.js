187c126d1fc04c8b6150b24959cbd53ed7ee8c4c
llandkoer <llandkoer@gmail.com>
2021-07-05 21:23:54 -0500

    Add posts (defaults)

--- api/src/database.js
+++ api/src/database.js
@@ -6,7 +6,7 @@ let db;
 async function createConnection() {
   const adapter = new FileAsync("db.json");
   db = await low(adapter);
-  db.defaults({ users: [], challenges: [], partners: [] }).write();
+  db.defaults({ users: [], challenges: [], partners: [], posts: [] }).write();
 }
 
 const getConnection = () => db;
