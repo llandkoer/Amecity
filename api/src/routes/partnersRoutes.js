bcf63e73565dcfd0adf63ba5153cc2ea6a7f2d50
Restyled.io <commits@restyled.io>
2021-07-06 23:41:12 +0000

    Restyled by prettier

--- api/src/routes/partnersRoutes.js
+++ api/src/routes/partnersRoutes.js
@@ -1,4 +1,4 @@
-const {Router} = require("express");
+const { Router } = require("express");
 
 const router = Router();
 
@@ -6,17 +6,19 @@ const partnersControllers = require("../controllers/partnersControllers");
 const partnersMiddlewares = require("../middlewares/partnersMiddlewares");
 const verifyToken = require("../middlewares/verifyToken");
 
-const {config} = require("../config/config");
+const { config } = require("../config/config");
 
 router.post(
-    "/create",
-    verifyToken.verifyToken(config.jwt.admin.admin, config.jwt.admin.label),
-    partnersMiddlewares.createPartnerValidations(),
-    partnersControllers.createPartner);
+  "/create",
+  verifyToken.verifyToken(config.jwt.admin.admin, config.jwt.admin.label),
+  partnersMiddlewares.createPartnerValidations(),
+  partnersControllers.createPartner
+);
 
 router.put(
-    "/updateRewards/:partner_id",
-    verifyToken.verifyToken(config.jwt.admin.admin, config.jwt.admin.label),
-    partnersControllers.updateRewards);
+  "/updateRewards/:partner_id",
+  verifyToken.verifyToken(config.jwt.admin.admin, config.jwt.admin.label),
+  partnersControllers.updateRewards
+);
 
 module.exports = router;
