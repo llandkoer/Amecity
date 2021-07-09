19e7af1801afc380ad7e112871ce08c9d2be06e9
Restyled.io <commits@restyled.io>
2021-07-09 01:22:15 +0000

    Restyled by prettier

--- api/src/routes/usersRoutes.js
+++ api/src/routes/usersRoutes.js
@@ -1,27 +1,30 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const router = Router();
 
 const usersController = require("../controllers/usersControllers");
 const usersMiddlewares = require("../middlewares/usersMiddlewares");
 const verifyToken = require("../middlewares/verifyToken");
 
-const {config} = require("../config/config");
+const { config } = require("../config/config");
 
 router.get(
-    "/getPoints",
-    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
-    usersController.getPoints);
+  "/getPoints",
+  verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+  usersController.getPoints
+);
 
 router.put(
-    "/redeemPoints",
-    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
-    usersMiddlewares.givePointsValidations(),
-    usersController.redimePoints,
-)
+  "/redeemPoints",
+  verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+  usersMiddlewares.givePointsValidations(),
+  usersController.redimePoints
+);
 
 router.put(
-    "/givePoints",
-    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
-    usersMiddlewares.givePointsValidations(), usersController.givePoints);
+  "/givePoints",
+  verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+  usersMiddlewares.givePointsValidations(),
+  usersController.givePoints
+);
 
 module.exports = router;
