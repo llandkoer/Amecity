78f9cc798a01a2460322279355eb1969f59ba1b1
Restyled.io <commits@restyled.io>
2021-07-10 16:22:22 +0000

    Restyled by prettier

--- api/src/middlewares/blogMiddlewares.js
+++ api/src/middlewares/blogMiddlewares.js
@@ -1,19 +1,19 @@
-const {check} = require("express-validator");
+const { check } = require("express-validator");
 
-exports.createPostValidations =
-    () => [check("title")
-               .notEmpty()
-               .withMessage("Title is required")
-               .isLength({min : 4, max : 100})
-               .withMessage("Title must be between 4 and 100 characters long"),
-           check("photo_url")
-               .notEmpty()
-               .withMessage("Photo URL is required")
-               .isURL()
-               .withMessage("Photo URL is not valid"),
-           check("information")
-               .notEmpty()
-               .withMessage("Information is required")
-               .isLength({min : 100})
-               .withMessage("Information must be at least 100 characters long"),
+exports.createPostValidations = () => [
+  check("title")
+    .notEmpty()
+    .withMessage("Title is required")
+    .isLength({ min: 4, max: 100 })
+    .withMessage("Title must be between 4 and 100 characters long"),
+  check("photo_url")
+    .notEmpty()
+    .withMessage("Photo URL is required")
+    .isURL()
+    .withMessage("Photo URL is not valid"),
+  check("information")
+    .notEmpty()
+    .withMessage("Information is required")
+    .isLength({ min: 100 })
+    .withMessage("Information must be at least 100 characters long"),
 ];
