9bb8899812f911f3716bd9329f311bc5268f23b9
Restyled.io <commits@restyled.io>
2021-07-10 17:04:21 +0000

    Restyled by prettier

--- api/src/controllers/challengesControllers.js
+++ api/src/controllers/challengesControllers.js
@@ -1,41 +1,46 @@
-const {nanoid} = require("nanoid");
-const {validationResult} = require("express-validator");
+const { nanoid } = require("nanoid");
+const { validationResult } = require("express-validator");
 
-const {getConnection} = require("../database");
+const { getConnection } = require("../database");
 
 const createChallenge = async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(422).json({errors : errors.array()});
+      return res.status(422).json({ errors: errors.array() });
     }
 
-    let {title, photo_url, description, points, focus, requirements} = req.body;
+    let { title, photo_url, description, points, focus, requirements } =
+      req.body;
 
     if (!Array.isArray(requirements)) {
-      return res.status(400).json({message : "Requirements must be an array"});
+      return res.status(400).json({ message: "Requirements must be an array" });
     }
 
     if (requirements.length < 1) {
-      return res.status(400).json({message : "Requirements must not be empty"});
+      return res
+        .status(400)
+        .json({ message: "Requirements must not be empty" });
     }
 
     for (let i = 0; i < requirements.length; i++) {
       const element = requirements[i];
       if (typeof element !== "string") {
-        return res.status(400).json(
-            {message : `Requirements on index ${i} is not a string`});
+        return res
+          .status(400)
+          .json({ message: `Requirements on index ${i} is not a string` });
       }
     }
 
     if (typeof points !== "number") {
-      return res.status(400).json({message : `Points is NaN`});
+      return res.status(400).json({ message: `Points is NaN` });
     }
 
-    const challenge = getConnection().get("challenges").find({title}).value();
+    const challenge = getConnection().get("challenges").find({ title }).value();
     if (challenge) {
-      return res.status(409).json(
-          {message : "Challenge already exist on our database"});
+      return res
+        .status(409)
+        .json({ message: "Challenge already exist on our database" });
     }
 
     const challenge_id = nanoid();
@@ -51,9 +56,11 @@ const createChallenge = async (req, res) => {
     };
     getConnection().get("challenges").push(newChallenge).write();
 
-    res.status(201).json({message : "Challenge has been created successfully"});
+    res
+      .status(201)
+      .json({ message: "Challenge has been created successfully" });
   } catch (error) {
-    res.status(500).json({error, message : "There was a server error"});
+    res.status(500).json({ error, message: "There was a server error" });
   }
 };
 
