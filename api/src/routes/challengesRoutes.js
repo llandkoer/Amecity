e47b4e13a2c7ce542f9c6f89ee5b7eac31c04d32
Restyled.io <commits@restyled.io>
2021-07-22 22:37:45 +0000

    Restyled by clang-format

--- api/src/routes/challengesRoutes.js
+++ api/src/routes/challengesRoutes.js
@@ -1,4 +1,4 @@
-const { Router } = require("express");
+const {Router} = require("express");
 
 const router = Router();
 
@@ -6,31 +6,40 @@ const challengeMiddlewares = require("../middlewares/challengesMiddlewares");
 const challengesController = require("../controllers/challengesControllers");
 const verifyToken = require("../middlewares/verifyToken");
 
-const { config } = require("../config/config");
+const {config} = require("../config/config");
 
 router.post(
-  "/create",
-  verifyToken.verifyToken(config.jwt.admin.admin, config.jwt.admin.label),
-  challengeMiddlewares.createChallengeValidation(),
-  challengesController.createChallenge,
+    "/create",
+    verifyToken.verifyToken(config.jwt.admin.admin, config.jwt.admin.label),
+    challengeMiddlewares.createChallengeValidation(),
+    challengesController.createChallenge,
 );
 
-router.get("/getAll", verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label), challengesController.getAllChallenges);
+router.get(
+    "/getAll",
+    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+    challengesController.getAllChallenges);
 
 router.post(
-  "/takeChallenge/:challenge_id",
-  verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
-  challengesController.takeChallenge,
+    "/takeChallenge/:challenge_id",
+    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+    challengesController.takeChallenge,
 );
 
 router.put(
-  "/achieveChallenge/:challenge_id",
-  verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
-  challengesController.achieveChallenge,
+    "/achieveChallenge/:challenge_id",
+    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+    challengesController.achieveChallenge,
 );
 
-router.get("/getAchieved", verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label), challengesController.getAchieved);
+router.get(
+    "/getAchieved",
+    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+    challengesController.getAchieved);
 
-router.get("/getCurrent", verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label), challengesController.getCurrent);
+router.get(
+    "/getCurrent",
+    verifyToken.verifyToken(config.jwt.player.admin, config.jwt.player.label),
+    challengesController.getCurrent);
 
 module.exports = router;
